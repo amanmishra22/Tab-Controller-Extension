9ca864986ca0821c9c339bacc63cda1e8b080907
Aman Mishra <104980147+amanmishra22@users.noreply.github.com>
2022-12-19 12:49:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const eleclose=document.getElementById("close");
+eleclose.onclick=()=>{
+    chrome.tabs.query({active:true,currentWindow: true},(tabs)=>{
+        const currentTab=tabs[0];
+        chrome.tabs.query({currentWindow:true},(tabs)=>{
+            tabs.map((tab)=>{
+                if(tab.id!=currentTab.id){
+                    chrome.tabs.remove(tab.id);
+                }
+            })
+        })
+    })
+}
